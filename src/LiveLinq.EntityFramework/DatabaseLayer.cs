9c8a19e90b5ab16253bc2b0a9041da326acaecf1
nloum <39776302+nloum@users.noreply.github.com>
2020-08-17 12:29:30 -0500

    Add fluent syntax for db layer

---
+++ src/LiveLinq.EntityFramework/DatabaseLayer.cs
@@ -0,0 +1,30 @@
+using System;
+using ComposableCollections.Dictionary;
+using Microsoft.EntityFrameworkCore;
+
+namespace LiveLinq.EntityFramework
+{
+    public static class DatabaseLayer
+    {
+        public static DatabaseLayer<TDbContext> Create<TDbContext>(Func<TDbContext> createDbContext) where TDbContext : DbContext
+        {
+            return new DatabaseLayer<TDbContext>(createDbContext);
+        }
+    }
+    
+    public class DatabaseLayer<TDbContext> where TDbContext : DbContext
+    {
+        private readonly Func<TDbContext> _create;
+
+        public DatabaseLayer(Func<TDbContext> create)
+        {
+            _create = create;
+        }
+
+        public IComposableDictionary<TId, TDbDto> WithAggregateRoot<TId, TDbDto>(Func<TDbContext, DbSet<TDbDto>> dbSet,
+            Func<TDbDto, TId> id) where TDbDto : class
+        {
+            return new AnonymousEntityFrameCoreDictionary<TId, TDbDto, TDbContext>(dbSet, _create, id);
+        }
+    }
+}
